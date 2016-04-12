82d285f785d4f6b60762f715c65b72753b227f8d
Tom Beck <tom@tastylounge.com>
2016-04-11 21:33:23 -0700

    added scroll animations and some organizational helpers

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,5 +1,17 @@
 //= require vendor/jquery-1.11.3.min
+//= require vendor/jquery.stickit.min
 //= require vendor/prettify
+
+$(window).on('load', function (e){
+  window.scrollTo(0, 0);
+  if (window.location.hash) {
+    $('html, body').animate({ scrollTop: $(window.location.hash).offset().top-80 }, 800, function (){
+      // Mark as active
+      $('a[href^="#"]').removeClass('active');
+      $('a[href="'+window.location.hash+'"]').addClass('active');
+    });
+  }
+});
 
 
 $(document).on('ready', function(){
@@ -12,4 +24,44 @@ $(document).on('ready', function(){
   $('.highlight').mouseleave(function(){
     $('#code-display p').text('Hover over code snippet on the left to learn more.');
   });
+
+
+  // Add scroll animations
+  $('a[href^="#"]').click(function() {
+      var target = $(this.hash);
+      var hash = this.hash;
+      if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+      if (target.length == 0) target = $('html');
+      $('html, body').animate({ scrollTop: target.offset().top-80 }, 500, function (){
+          location.hash = hash;
+      });
+      return false;
+  });
+
+  // initiate stick behaviour
+  $(".sticky").stickit({
+    // scope: StickScope.Parent,
+    top: 50
+  });
+  $("#pagenav").stickit({
+    // scope: StickScope.Parent,
+    top: 70
+  });
+  $("#subnav").stickit({
+    // scope: StickScope.Parent,
+    top: 0
+  });
+
+});
+
+
+// When a user scrolls to 50px add class  condensed-header to body
+$(window).scroll(function(){
+  var currentScreenPosition  = $(document).scrollTop();
+
+  if(currentScreenPosition > 50) {
+    $('body').addClass('condensed-header');
+  } else {
+    $('body').removeClass('condensed-header');
+  }
 });
