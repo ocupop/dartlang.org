bd871c27b9fa68978280d5a2519a87b5fc897855
Tom Beck <tom@tastylounge.com>
2016-04-26 15:54:43 -0700

    updated toggle for sidebar navigation

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -21,12 +21,17 @@ $(document).on('ready', function(){
   $('.highlight').mouseenter(function(){
     var text = $(this).data('text');
     $('#code-display p').text(text);
-    window.console.log(text);
+    // window.console.log(text);
   });
   $('.highlight').mouseleave(function(){
     $('#code-display p').text('Hover over code snippet on the left to learn more.');
   });
 
+  $('#toc i').on('click', function(e) {
+    window.console.log("CLICKED");
+    e.preventDefault();
+    $(this).parent('li').toggleClass('open');
+  });
 
   // Add scroll animations
   $('a[href^="#"]').click(function() {
@@ -40,25 +45,37 @@ $(document).on('ready', function(){
       return false;
   });
 
-  // initiate stick behaviour
-  $(".sticky").stickit({
-    top: 50
-  });
+  // // initiate stick behaviour
+  // $(".sticky").stickit({
+  //   top: 50
+  // });
   $("#toc").stickit({
     top: 50
   });
-  $("#pagenav").stickit({
-    top: 60
-  });
-  $("#subnav").stickit({
-    top: 0
+  // $("#pagenav").stickit({
+  //   top: 60
+  // });
+  // $("#subnav").stickit({
+  //   top: 0
+  // });
+
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
   });
 
-  $('.dropdown-toggle').on('click', function(e) {
-    e.preventDefault();
-    $(this).parent().toggleClass('open');
-  });
+});
 
+$(document).ready(function() {
+  $.getJSON(
+    "https://storage.googleapis.com/dart-archive/channels/dev/release/latest/VERSION",
+    function( data ) {
+        var date = data.date;
+        var revDate = date.substr(0,4) + "-" + date.substr(4,2) + "-" + date.substr(6,2);
+        $(".dev-channel").append($("<strong></strong>").text("version " +data.version))
+                         .append($("<span></span>").text(", built on " + revDate))
+                         .append($("<span></span>").text(", at revision " + data.revision));
+  });
 });
 
 
