dd295169bce6a5bb0ed04e750b565697a4b078c0
Filip Hracek <filip.hracek@gmail.com>
2016-07-07 14:52:26 -0700

    Check actual HTML, on firebase, with redirects and all that

--- deploy/html_proof.rb
+++ deploy/html_proof.rb
@@ -1,45 +1,52 @@
 #!/usr/bin/env ruby
 
 require 'html-proofer'
-require 'nokogiri'
+require 'net/http'
+require 'tempfile'
 
-require_relative 'old_site_urls'
+require_relative 'urls/get_all'
 
-DOMAIN = 'https://www.dartlang.org/'
-LOCALHOST = 'http://localhost:4000/'
-
-puts "===== Checking links, redirects and HTML through HTMLProofer ====="
-
-puts "Parsing current sitemap.xml for all current URLs"
-sitemap = File.open("publish/sitemap.xml") { |f| Nokogiri::XML(f) }
-urls = sitemap.xpath("//xmlns:loc").map { |node| node.content }
-
-puts "Adding old site's sitemap URLs"
-urls = urls | $OLD_SITE_URLS  # does not add URLs that already exist
-
-# Change dartlang.org to localhost:4000
-localhost_urls = urls.map { |url| url.sub(DOMAIN, LOCALHOST) }
+puts "===== Checking HTML and outbound links and through HTMLProofer ====="
 
 # TODO(filiph) remove when not needed
-localhost_urls.delete("http://localhost:4000/events/2016/summit/index.html")
+$LOCALHOST_URLS.delete("http://localhost:4000/events/2016/summit/index.html")
 
 puts "Spawning firebase server on localhost"
-pid = spawn("firebase serve --port 4000", :out => "/dev/null")
+pid = spawn("firebase serve --port #{$PORT}", :out => "/dev/null")
 puts "..."
 sleep 5
 
 begin
-  puts "Checking links"
-  HTMLProofer.check_links(localhost_urls, {
-    :check_external_hash => true,
-    :check_favicon => true,
-    :check_html => true,
-    :validation => {
-      :report_invalid_tags => true,
-      :report_missing_names => true
-    },
-    :log_level => :warn
-  }).run
+  puts "Checking contents"
+  $LOCALHOST_URLS.each do |url|
+    uri = URI(url)
+    puts "- #{uri}"
+    source = Net::HTTP.get(uri)
+    file = Tempfile.new([uri.path.gsub(/\//, '___') + '_____', '.html'])
+    begin
+      file.write(source)
+    ensure
+      file.close
+    end
+
+    begin
+      HTMLProofer.check_file(file.path, {
+        :url_swap => { /^\/(.*)/ => "#{$LOCALHOST}\\1" },
+        :check_external_hash => true,
+        # :check_favicon => true,
+        :check_html => true,
+        :validation => {
+          # :report_missing_names => true
+        },
+        :cache => {
+          :timeframe => '7d'
+        },
+        :log_level => :warn
+      }).run
+    ensure
+       file.unlink   # deletes the temp file
+    end
+  end
 ensure
   puts "Killing firebase server on localhost"
   Process.kill(:SIGINT, pid)
---
+++ deploy/urls/get_all.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+
+require_relative 'old_site_urls'
+
+$PORT = 4000
+$DOMAIN = 'https://www.dartlang.org/'
+$LOCALHOST = "http://localhost:#{$PORT}/"
+
+puts "Parsing current sitemap.xml for all current URLs"
+sitemap = File.open("publish/sitemap.xml") { |f| Nokogiri::XML(f) }
+urls = sitemap.xpath("//xmlns:loc").map { |node| node.content }
+
+puts "Adding old site's sitemap URLs"
+$URLS = urls | $OLD_SITE_URLS  # does not add URLs that already exist
+
+# Change dartlang.org to localhost:4000
+$LOCALHOST_URLS = urls.map { |url| url.sub($DOMAIN, $LOCALHOST) }
