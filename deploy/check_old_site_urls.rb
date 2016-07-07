823cc9c83f41249facbd1cd38f837e9c8557cf92
Filip Hracek <filip.hracek@gmail.com>
2016-07-07 00:59:01 -0700

    Use the same port (4000) with HTMLProofer as with jekyll serve

--- deploy/check_old_site_urls.rb
+++ deploy/check_old_site_urls.rb
@@ -4,13 +4,13 @@ require 'html-proofer'
 require_relative 'old_site_urls'
 
 DOMAIN = 'https://www.dartlang.org/'
-LOCALHOST = 'http://localhost:5000/'
+LOCALHOST = 'http://localhost:4000/'
 
 localhost_urls = $OLD_SITE_URLS.map { |url| url.sub(DOMAIN, LOCALHOST) }
 
 puts "===== Checking all links from old site's sitemap.xml ====="
 puts "Spawning firebase server on localhost"
-pid = spawn("firebase serve --port 5000", :out => "/dev/null")
+pid = spawn("firebase serve --port 4000", :out => "/dev/null")
 puts "..."
 sleep 5
 
